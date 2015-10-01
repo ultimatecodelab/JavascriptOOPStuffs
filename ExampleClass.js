2b3befecedf53f851f24e78fd2cd8c7f2f641938
Arjun Kharel <adgn353@gmail.com>
2015-10-01 19:45:15 +0100

    Js OOP
    js prototype

---
+++ ExampleClass.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Arjun Kharel on 10/1/2015.
+ */
+function ExampleClass(){
+    //Basic class defination in Javascript
+    this.square = function SquareNumber(arg1){
+        return arg1*arg1;
+    }
+}
+//creating the instance of ExampleClass
+var instance1= new ExampleClass();
